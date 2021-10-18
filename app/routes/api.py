d59831d943e1e42035994d95edbfa2f8b47e5551
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-10-18 14:40:44 -0700

    add login and password validation

--- app/routes/api.py
+++ app/routes/api.py
@@ -32,3 +32,28 @@ def signup ():
   session['user_id'] = newUser.id
   session['loggedIn'] = True
   return jsonify(id = newUser.id)
+
+@bp.route('users/logout', methods=['POST'])
+def logout():
+  #remove session variablies
+  session.clear()
+  return '', 204
+
+@bp.route('/users/login', methods=['POST'])
+def login():
+  data = request.get_json(0)
+  db = get_db()
+
+  try:
+    user = db.query(User).filter(User.email == data['email']).one()
+    if user.verify_password(data['password']) == False:
+      return jsonify(message = 'Incorrect credentials'), 400
+    session.clear()
+    session['user_id'] = user.id
+    session['loggedIn'] = True
+
+    return jsonify(id = user.id)
+  except:
+    print(sys.exc_info()[0])
+
+    return jsonify(message = 'Incorrect credentials'), 400
